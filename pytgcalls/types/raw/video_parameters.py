e8693c95e5e0fb29d69cb646de9e77e5d1e06124
hakutakaid <rachmad.alfio@gmail.com>
2024-06-25 12:48:59 +0000

    pytgcalls

---
+++ pytgcalls/types/raw/video_parameters.py
@@ -0,0 +1,19 @@
+from ...statictypes import statictypes
+from ..py_object import PyObject
+from ..stream.video_quality import VideoQuality
+
+
+class VideoParameters(PyObject):
+    @statictypes
+    def __init__(
+        self,
+        width: int = 640,
+        height: int = 360,
+        frame_rate: int = 20,
+    ):
+        max_w, max_h, max_fps = max(
+            VideoQuality, key=lambda x: x.value[0],
+        ).value
+        self.width: int = min(width, max_w)
+        self.height: int = min(height, max_h)
+        self.frame_rate: int = min(frame_rate, max_fps)
